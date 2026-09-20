8752407018153cae56efa13c8c85493b928132a5
joshua-data <joshuajkim413@gmail.com>
2026-09-20 17:48:06 +0900

    edited 5. OOP

--- PythonRefresher/OOP/5. OOP/main.py
+++ PythonRefresher/OOP/5. OOP/main.py
@@ -1,24 +1,23 @@
+from Enemy import *
 from Zombie import *
 from Ogre import *
-from Enemy import *
 
 def battle(e1: Enemy, e2: Enemy):
     e1.talk()
     e2.talk()
 
-    while e1.health_points > 0 and e2.health_points > 0:
-        print("-------------")
+    while e1.health_points > 0 and e1.health_points > 0:
+        print("=================")
         e1.special_attack()
         e2.special_attack()
-        print(f"{e1.get_type_of_enemy()}: {e1.health_points} HP left")
-        print(f"{e2.get_type_of_enemy()}: {e2.health_points} HP left")
+        print(f"{e1.get_type_of_enemy()}: {e1.health_points} HP left.")
+        print(f"{e2.get_type_of_enemy()}: {e2.health_points} HP left.")
         e2.attack()
         e1.health_points -= e2.attack_damage
         e1.attack()
         e2.health_points -= e1.attack_damage
 
-    print("-------------")
-
+    print("=================")
     if e1.health_points > 0:
         print(f"{e1.get_type_of_enemy()} wins!")
     else:
@@ -29,5 +28,3 @@ zombie = Zombie(10, 1)
 ogre = Ogre(20, 3)
 
 battle(zombie, ogre)
-
-
